d5eee85d4184085c0b693f56c99ab9868f6a944e
Sunil Nimmagadda <sunil@nimmagadda.net>
2016-04-09 22:01:56 +0530

    Let the server accept more than 1 client.

--- echo_server.lua
+++ echo_server.lua
@@ -1,5 +1,6 @@
 local net = require("net")
 local tls = require("ltls")
+local unix = require("unix")
 
 local t = {
 	["cert"] = "/home/sunil/lualtls/obj/server.crt",
@@ -8,17 +9,27 @@ local t = {
 };
 local config = assert(tls.config_new(t))
 local s = assert(net.bind("localhost", "1234"))
-ctx = assert(tls.accept(s:accept():socket(), config))
-
-::read_again::
-buf, r = assert(ctx:read(1024))
-if r == tls.WANT_POLLIN or r == tls.WANT_POLLOUT then
-	goto read_again
+unix.signal(unix.SIGINT, function() os.exit(0) end);
+while true do
+	local s2 = s:accept()
+	ctx = assert(tls.accept(s2:socket(), config))
+	if unix.fork() == 0 then
+		::read_again::
+		buf, r = assert(ctx:read(1024))
+		if r == tls.WANT_POLLIN or r == tls.WANT_POLLOUT then
+			goto read_again
+		end
+		io.write(buf)
+		::write_again::
+		_, r = assert(ctx:write(buf))
+		if r == tls.WANT_POLLIN or r == tls.WANT_POLLOUT then
+			goto write_again
+		end
+		assert(ctx:close())
+		s2:close()
+		os.exit(0)
+	else
+		s2:close()
+	end
 end
-io.write(buf)
-
-::write_again::
-_, r = assert(ctx:write(buf))
-if r == tls.WANT_POLLIN or r == tls.WANT_POLLOUT then
-	goto write_again
-end
+s:close()
