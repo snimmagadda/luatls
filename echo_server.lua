c06619a1443e19d24ed43ae5973939e4dc489ac4
Sunil Nimmagadda <sunil@nimmagadda.net>
2016-04-09 20:48:24 +0530

    assert around methods.

--- echo_server.lua
+++ echo_server.lua
@@ -11,14 +11,14 @@ local s = assert(net.bind("localhost", "1234"))
 ctx = assert(tls.accept(s:accept():socket(), config))
 
 ::read_again::
-buf, r = ctx:read(1024)
+buf, r = assert(ctx:read(1024))
 if r == tls.WANT_POLLIN or r == tls.WANT_POLLOUT then
 	goto read_again
 end
 io.write(buf)
 
 ::write_again::
-_, r = ctx:write(buf)
+_, r = assert(ctx:write(buf))
 if r == tls.WANT_POLLIN or r == tls.WANT_POLLOUT then
 	goto write_again
 end
