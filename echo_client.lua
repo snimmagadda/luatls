b0cf6f8812e9581977d22ca75eee1a408e787463
Sunil Nimmagadda <sunil@nimmagadda.net>
2016-04-14 10:20:35 +0530

    Lua is not C.

--- echo_client.lua
+++ echo_client.lua
@@ -5,9 +5,9 @@ local t = {
 	["cert"] = "/home/sunil/lualtls/obj/server.crt",
 	["key"] = "/home/sunil/lualtls/obj/server.key",
 	["ca"] = "/home/sunil/lualtls/obj/ca.crt"
-};
+}
 local config = assert(tls.config_new(t))
-local ctx = assert(tls.connect("localhost", "1234", config));
+local ctx = assert(tls.connect("localhost", "1234", config))
 aux.write(ctx, "hello world")
 buf = aux.read(ctx, 1024)
 io.write(buf)
--- echo_server.lua
+++ echo_server.lua
@@ -7,10 +7,10 @@ local t = {
 	["cert"] = "/home/sunil/lualtls/obj/server.crt",
 	["key"] = "/home/sunil/lualtls/obj/server.key",
 	["ca"] = "/home/sunil/lualtls/obj/ca.crt"
-};
+}
 local config = assert(tls.config_new(t))
 local s = assert(net.bind("localhost", "1234"))
-unix.signal(unix.SIGINT, function() os.exit(0) end);
+unix.signal(unix.SIGINT, function() os.exit(0) end)
 while true do
 	local s2 = s:accept()
 	ctx = assert(tls.accept(s2:socket(), config))
